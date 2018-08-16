dfff8ff7c14dee9eab9ab591d545a75a28753e7f
jjbby <jchristie1@gmail.com>
2018-08-16 22:55:44 +1000

    Code Challenge 3 refactored

--- challenges/array_binary_search/README.md
+++ challenges/array_binary_search/README.md
@@ -7,4 +7,6 @@ This takes a sorted array, finds the middle of the array and and checks if that 
 Write a function called BinarySearch which takes in 2 parameters: a sorted array and the search key. Without utilizing any of the built-in methods available to your language, return the index of the array’s element that is equal to the search key, or -1 if the element does not exist.
 ## Solution
 <!-- Embedded whiteboard image -->
+Question: Is 3 in [1, 2, 3]?<br>
+Answer: Yes, t index of 2 <br>
 ![title](https://github.com/401d9/data_structures_and_algorithms/blob/array-binary-search/assets/array_binary_search.jpg)
--- challenges/array_binary_search/array_binary_search.py
+++ challenges/array_binary_search/array_binary_search.py
@@ -1,32 +1,59 @@
-from bisect import bisect_left
+# from bisect import bisect_left
+#
+#
+# def binary_search(a, x, lo=0, hi=None):
+#     """
+#        Binary search for an item in a sorted list.
+#
+#
+#        Args:
+#            a:  A sorted list
+#            x:  The item to search for
+#            lo: (Pre-set value) The lowest index to start in the passed in list
+#            hi: (Pre-set value) This starts at None to check that list has a value. It then checks length for the end of
+#             the list
+#        Returns:
+#            True or False
+#         Bisect:
+#             lo=0 declares where to start in the array
+#             hi=gets set to the length of the array
+#        """
+#     hi = hi if hi is not None else len(a)
+#     pos = bisect_left(a, x, lo, hi)
+#     return pos if pos != hi and a[pos] == x else -1
+#
+#
+# """
+# Credits:
+#    Docstring example: https://codereview.stackexchange.com/questions/147136/binary-search-a-sorted-list-of-integers
+#    Bisection example: https://stackoverflow.com/questions/212358/binary-search-bisection-in-python
+# """
+numb_array = [1, 2, 3, 5, 6, 7, 8, 9]
 
 
-def binary_search(a, x, lo=0, hi=None):
-    """
-       Binary search for an item in a sorted list.
-
-        Takes in only a and x and provides lo and hi arguments
-       Args:
-           a:     A sorted list
-           x:  The item to search for
-       Returns:
-           True or False
-        Bisect:
-            lo=0 declares where to start in the array
-            hi=gets set to the length of the array
-       """
-    hi = hi if hi is not None else len(a)
-    pos = bisect_left(a, x, lo, hi)
-    return pos if pos != hi and a[pos] == x else -1
+# print(binary_search(numb_array, 1))
+# print(binary_search(numb_array, 4))
+# print(binary_search(numb_array, 9))
 
 
-"""
-Credits:
-   Docstring example: https://codereview.stackexchange.com/questions/147136/binary-search-a-sorted-list-of-integers
-   Bisection example: https://stackoverflow.com/questions/212358/binary-search-bisection-in-python
-"""
-numb_array=[1, 2, 3, 5, 6, 7, 8, 9]
-print(binary_search(numb_array, 1))
-print(binary_search(numb_array, 4))
-print(binary_search(numb_array, 9))
+def binary_search(array_arg, search_numb):
+    first = 0
+    last = len(array_arg) - 1
+    found = False
 
+    while first <= last and not found:
+        mid_point = (first + last) // 2
+        if array_arg[mid_point] == search_numb:
+            found = mid_point
+        else:
+            if search_numb < array_arg[mid_point]:
+                last = mid_point - 1
+            else:
+                first = mid_point + 1
+    if found == False:
+        found = -1
+    return found
+
+
+x = int(input('input: '))
+print(binary_search(numb_array, x))
