2b1882d72396a63ce05b2c3b3e892557fc7b5937
jjbby <jchristie1@gmail.com>
2018-08-16 16:29:18 +1000

    Updating the challenge to be almost complete

--- challenges/array_shift/array_shift.py
+++ challenges/array_shift/array_shift.py
@@ -1,5 +1,7 @@
 def insert_shift_array(arr, val):
-    """This is my first docstring.
-     This function is taking in an array and adding another argument to the middle of that array"""
+    """
+    This is my first docstring.
+    This function is taking in an array and adding another argument to the middle of that array
+     """
     ind = len(arr) // 2 + len(arr) % 2
     return arr[:ind] + [val] + arr[ind:]
