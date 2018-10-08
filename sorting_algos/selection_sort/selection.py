a4121f2ae9c7f2f22f3f465613e1f064a279829b
J <jchristie1@gmail.com>
2018-10-07 20:21:30 -0700

    Added test for Selection sort

--- sorting_algos/selection_sort/selection.py
+++ sorting_algos/selection_sort/selection.py
@@ -1,35 +1,15 @@
-import random
-
-
-def really_big_list(numb):
-    '''
-    This creates a list with randome numbers put into a list to use for testing sorting
-    
-    ARG:
-        Input: Number for length of random list
-    OUTPUT:
-        Returns a list with input number length. 
-    '''
-
-    big_list = []
-    counter = numb
-    while counter:
-        ran_num = random.randint(0, numb)
-        big_list.append(ran_num)
-        counter -= 1
-
-    return big_list
-
-
 def selection_sort(unsorted):
-    '''
-    Selection sort takes a list and sorts if from lowest to greatest value. For each index check the value at that index against all the unchecked indexes for the lowest value and swap the current index value for the lowest value found. Example: [2, 5, 3] will become [2, 3, 5] via [2, 5, 3] -> [2, 3, 5] -> [2, 3, 5] which goes through the whole list. 
+    """
+    Selection sort takes a list and sorts if from lowest to greatest value. For each index check the value at that
+    index against all the unchecked indexes for the lowest value and swap the current index value for the lowest
+    value found. Example: [2, 5, 3] will become [2, 3, 5] via [2, 5, 3] -> [2, 3, 5] -> [2, 3, 5] which goes through
+    the whole list.
 
     ARG:
         An unsorted list
     OUTPUT:
         A value sorted list
-    '''
+    """
 
     for i in range(len(unsorted)):  # This gives you the Index of the list over the Value
         need_low_value = unsorted[i]
@@ -44,4 +24,4 @@ def selection_sort(unsorted):
             
     return unsorted
 
-print(selection_sort(really_big_list(100)))
+
