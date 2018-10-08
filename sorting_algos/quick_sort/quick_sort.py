c3b564203e9a37b1ee3893c18a962f159c7d5247
J <jchristie1@gmail.com>
2018-10-07 20:52:48 -0700

    Quick sort with tests added

--- sorting_algos/quick_sort/quick_sort.py
+++ sorting_algos/quick_sort/quick_sort.py
@@ -1,1 +1,20 @@
-# Implement the quick sort!
+
+
+def quick_sort(arg_list):
+    """ Function for implementing quick sort. """
+
+    len_check = len(arg_list)
+
+    if len_check <= 1:
+        return arg_list
+    else:
+        pivot = arg_list[0]
+        right = [i for i in arg_list[1:] if i > pivot]
+        left = [i for i in arg_list[1:] if i <= pivot]
+
+    return quick_sort(left) + [pivot] + quick_sort(right)
+
+
+sample_test_list = [5, 4, 2, 1, 0]
+
+print(quick_sort(sample_test_list))
